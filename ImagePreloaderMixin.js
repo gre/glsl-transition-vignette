a76d83dfce05188d5507ad5234cff69eccd747d0
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-04-06 14:12:06 +0200

    ImagePreloaderMixin has global cache

--- ImagePreloaderMixin.js
+++ ImagePreloaderMixin.js
@@ -1,12 +1,9 @@
 
 // Inspired by react-dnd
-
-var TRANSPARENT_PIXEL_SRC = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+var _cachedImages = {}, _readyImages = {};
 
 var ImagePreloaderMixin = {
   componentDidMount() {
-    this._cachedImages = {};
-    this._readyImages = {};
     this.preloadImages();
   },
 
@@ -14,25 +11,18 @@ var ImagePreloaderMixin = {
     this.preloadImages();
   },
 
-  componentWillUnmount() {
-    for (var key in this._cachedImages) {
-      this._cachedImages[key].src = TRANSPARENT_PIXEL_SRC;
-    }
-    this._cachedImages = {};
-  },
-
   hasPreloadedImage (url) {
-    return this._readyImages && !!this._readyImages[url];
+    return _readyImages && !!_readyImages[url];
   },
 
   getPreloadedImage (url) {
     if (this.hasPreloadedImage(url)) {
-      return this._cachedImages[url];
+      return _cachedImages[url];
     }
   },
 
   preloadImages() {
-    if (!this._cachedImages) return false;
+    if (!_cachedImages) return false;
     var urls = this.getImageUrlsToPreload();
     var allLoaded = true;
     for (var i=0; i<urls.length; ++i) {
@@ -44,24 +34,21 @@ var ImagePreloaderMixin = {
   },
 
   preloadImage (url) {
-    if (!url || this._cachedImages[url]) {
+    if (!url || _cachedImages[url]) {
       return;
     }
     var img = new window.Image();
     img.onload = function () {
+      this._readyImages[url] = true;
       if (this.isMounted()) {
-        this._readyImages[url] = true;
         this.forceUpdate();
       }
     }.bind(this);
     img.onerror = function () {
-      if (this.isMounted()) {
-        delete this._cachedImages[url];
-      }
+      delete _cachedImages[url];
     }.bind(this);
     img.src = url;
-
-    this._cachedImages[url] = img;
+    _cachedImages[url] = img;
   }
 };
 
